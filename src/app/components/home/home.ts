25cbe8dff79f6a9e5355469320929d14c4f3842a
bytauro3000 <bytauro3000@gmail.com>
2026-07-28 16:47:24 -0500

    fix ruta environment en home.ts

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingComponent } from '../landing/landing.component';
 import { LandingMerruicComponent } from '../landing-merruic/landing-merruic';
-import { environment } from '../../environments/environment';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-home',
