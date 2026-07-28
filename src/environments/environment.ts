570a7fe857bb0431e72ed7147f8a5e979a08c1ed
bytauro3000 <bytauro3000@gmail.com>
2026-07-28 14:46:43 -0500

    environment revertido a ivan + set-env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://ms-gateway-latest.onrender.com',
-    wsUrl: 'wss://ms-gateway-latest.onrender.com'
+    apiUrl: 'https://gatewayinmobiliaria.onrender.com',
+    wsUrl: 'wss://gatewayinmobiliaria.onrender.com'
 };
